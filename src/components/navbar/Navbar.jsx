39f3ffb7a5696f020120a2462db34909b0ea5edb
Tizazab <tizazabayana2@gmail.com>
2025-04-16 10:59:06 -0700

    Edited link on mobile

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className='fixed left-0 right-0 top-0 w-full backdrop-blur-2xl z-50'>
+    <div className='fixed left-0 right-0 top-0 w-full backdrop-blur-2xl z-50 '>
 
       <div className='max-w-7xl mx-auto h-[80px] flex justify-between items-center px-4 text-gray-300'>
         {/* LOGO */}
@@ -45,11 +45,11 @@ const Navbar = () => {
 
         {/* mobile menu */}
         <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-          <li className='py-6 text-4xl cursor-pointer hover:text-[#64ffda] duration-300'>Home</li>
-          <li className='py-6 text-4xl cursor-pointer hover:text-[#64ffda] duration-300'>About</li>
-          <li className='py-6 text-4xl cursor-pointer hover:text-[#64ffda] duration-300'>Skills</li>
-          <li className='py-6 text-4xl cursor-pointer hover:text-[#64ffda] duration-300'>Projects</li>
-          <li className='py-6 text-4xl cursor-pointer hover:text-[#64ffda] duration-300'>Contact</li>
+          <a href="#home"> <li className='py-6 text-4xl cursor-pointer hover:text-[#64ffda] duration-300'>Home</li></a>
+          <a href='#about'> <li className='py-6 text-4xl cursor-pointer hover:text-[#64ffda] duration-300'>About</li></a> 
+          <a href='#services'> <li className='py-6 text-4xl cursor-pointer hover:text-[#64ffda] duration-300'>Services</li></a>
+          <a href="#projects"><li className='py-6 text-4xl cursor-pointer hover:text-[#64ffda] duration-300'>Projects</li></a>
+          <a href="#contact"><li className='py-6 text-4xl cursor-pointer hover:text-[#64ffda] duration-300'>Contact</li></a> 
         </ul>
       </div>
     </div>
